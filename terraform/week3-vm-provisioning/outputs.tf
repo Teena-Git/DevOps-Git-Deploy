79bbfe684366d26528691dc12a7b8b7fee70080c
Teena-Git <2021wa86987@wilp.bits-pilani.ac.in>
2025-09-17 23:14:40 +0530

    Added output.tf to expose EC2 public IP

--- terraform/week3-vm-provisioning/outputs.tf
+++ terraform/week3-vm-provisioning/outputs.tf
@@ -1,4 +1,5 @@
 output "instance_public_ip" {
   description = "The public IP address of the EC2 instance"
-  value       = aws_instance.web.public_ip
+  value       = aws_instance.vm.public_ip
 }
+
