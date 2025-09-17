d738c65b25a4c31490fabc8ecc23777722f71d6d
Teena-Git <2021wa86987@wilp.bits-pilani.ac.in>
2025-09-18 01:21:21 +0530

    Added AWS provider block to main.tf

--- terraform/week3-vm-provisioning/main.tf
+++ terraform/week3-vm-provisioning/main.tf
@@ -3,8 +3,9 @@ provider "aws" {
 }
 resource "aws_instance" "vm" {
   ami           = "ami-0a716d3f3b16d290c"  
-  instance_type = "t2.micro"
+  instance_type = "t3.micro"
   tags = {
     Name = "Week3VM"
   }
 }
+
